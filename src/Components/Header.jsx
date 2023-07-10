e3d2011c17a5a83771b8feb2c306cc1e7e10c4a1
yisus245 <muozandres5@gmail.com>
2023-07-10 00:41:19 -0500

    entrega

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { colors } from '../Global/Colors'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from "../Global/Colors";
 
 const Header = () => {
   return (
-    <View 
-        style={styles.containerHeader}>
-      <Text style ={styles.text}>Header</Text>
+    <View style={styles.containerHeader}>
+      <Text style={styles.text}>Tienda de Ropa</Text>
+      <Text style={styles.text}>✰Fashion shop✰</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-    containerHeader: {
-        height: '10%',
-        backgroundColor: colors.peach,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 25,
-        fontFamily: 'Josefin'
-    }
-})
+  containerHeader: {
+    height: "10%",
+    backgroundColor: "#DEE5E6",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 25,
+    fontFamily: "Josefin",
+  },
+});
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,30 +1,33 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { colors } from '../Global/Colors'
-import categories from '../Data/categories.json'
-import CategoryItem from '../Components/CategoryItem'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from "../Global/Colors";
+import categories from "../Data/categories.json";
+import CategoryItem from "../Components/CategoryItem";
 
-const Home = ({
-  setCategorySelected
-}) => {
+const Home = ({ setCategorySelected }) => {
   return (
     <View style={styles.container}>
-        <FlatList
-            data = {categories}
-            keyExtractor={category => category}
-            renderItem={({item}) => CategoryItem({item, setCategorySelected})}
-            showsVerticalScrollIndicator={false}
-        />
+      <Text style={styles.Text}>Catalogo de Articulos</Text>
+      <FlatList
+        data={categories}
+        keyExtractor={(category) => category}
+        renderItem={({ item }) => CategoryItem({ item, setCategorySelected })}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        height: '90%',
-        backgroundColor: colors.lightPink,
-        alignItems: 'center'
-    }
-})
+  container: {
+    height: "90%",
+    backgroundColor: colors.lightPink,
+    alignItems: "center",
+  },
+  Text: {
+    fontSize: 25,
+    fontFamily: "Josefin",
+  },
+});
